4f5fa5c2d5f8ff33bd8e4394b7d408e5198dd25e
Truong Nguyen <truong.nguyen@tymlez.com>
2022-11-08 12:55:49 +1100

    enable dynamic SERVICE_CHANNEL for worker & ipfs

--- services/modules/helm-charts/charts/guardian-extensions/files/entrypoint.sh
+++ services/modules/helm-charts/charts/guardian-extensions/files/entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -ex
+ts=$(date +%s.%N)
+
+if [ "$SERVICE_CHANNEL" = "worker" ];
+then
+  export SERVICE_CHANNEL="worker.$ts"
+fi
+
+if [ "$SERVICE_CHANNEL" = "ipfs-client" ];
+then
+  export SERVICE_CHANNEL="ipfs-client.$ts"
+fi
+
 case "$ENABLE_APM_NAME" in
     "newrelic")
         echo "Installing newrelic dependencies"
