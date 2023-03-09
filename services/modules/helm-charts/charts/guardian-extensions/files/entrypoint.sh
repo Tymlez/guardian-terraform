e58e44597ace2914d1bc55849c52b26cc031a538
Truong Nguyen <truong.nguyen@tymlez.com>
2023-03-09 14:00:21 +1100

    enable guardian 2.10

--- services/modules/helm-charts/charts/guardian-extensions/files/entrypoint.sh
+++ services/modules/helm-charts/charts/guardian-extensions/files/entrypoint.sh
@@ -4,22 +4,25 @@ ts=$(date +%s.%N)
 
 if [ "$SERVICE_CHANNEL" = "worker" ];
 then
-  export SERVICE_CHANNEL="worker.$ts"
+    export SERVICE_CHANNEL="worker.$ts"
 fi
 
 if [ "$SERVICE_CHANNEL" = "ipfs-client" ];
 then
-  export SERVICE_CHANNEL="ipfs-client.$ts"
+    export SERVICE_CHANNEL="ipfs-client.$ts"
 fi
 
 case "$ENABLE_APM_NAME" in
     "newrelic")
         echo "Installing newrelic dependencies"
-
-        npm install --save-dev --legacy-peer-deps newrelic @newrelic/native-metrics
+        
+        mv package.json package.bak
+        npm install --legacy-peer-deps newrelic @newrelic/native-metrics
+        mv package.bak package.json
+        
         node -r newrelic ./dist/index.js
     ;;
     *)
-      node ./dist/index.js
+        node ./dist/index.js
     ;;
 esac
