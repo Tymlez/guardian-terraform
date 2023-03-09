9f8f30fae9ac116b57f87ff0dd6edbdba57cd1bb
Truong Nguyen <truong.nguyen@tymlez.com>
2023-03-09 15:58:35 +1100

    enable guardian 2.10

--- services/modules/helm-charts/charts/guardian-extensions/files/index.js
+++ services/modules/helm-charts/charts/guardian-extensions/files/index.js
@@ -1,13 +1,13 @@
 "use strict";
-const natsModule = require('/usr/common/node_modules/nats');
+const natsModule = require("/usr/local/common/node_modules/nats");
 
 // load the agent
-const newrelic = require('/usr/common/node_modules/newrelic');
+const newrelic = require("/usr/local/common/node_modules/newrelic");
 
 // instrument express after the agent has been loaded
 newrelic.instrumentLoadedModule(
-  'nats',    // the module's name, as a string
+  "nats", // the module's name, as a string
   natsModule // the module instance
 );
 
-require('./index')
+require("./index");
